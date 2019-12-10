c35ea76cadd25d01c6e37cf115bec7d69f4cd005
mustaffxx <felipeamorimw@gmail.com>
2019-12-10 14:36:02 -0300

    Ethernet Comm v2

--- EthernetCommunication/EthernetCommunication.ino
+++ EthernetCommunication/EthernetCommunication.ino
@@ -1,24 +1,30 @@
 #include <UIPEthernet.h> //INCLUSÃO DE BIBLIOTECA
+#include <Servo.h>
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //ATRIBUIÇÃO DE ENDEREÇO MAC AO ENC28J60
 byte ip[] = { 192, 168, 0, 25 };
 EthernetServer server(80);
 
-int ledPin = 8;
+Servo esc;
+const int escPin = 6;
+int spd = 0;
+int spdEsc = 0;
 
 String readString;
-int status = 0;
-int pos1 = 0;
 
 void setup() {
+  Serial.begin(9600);
+  esc.attach(escPin);
+  delay(1000);
+
   Ethernet.begin(mac, ip);
   server.begin();
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+
+  
 }
 void loop() {
 
-  byte brightness;
+
   EthernetClient client = server.available();
   if (client) { //
     while (client.connected()) {
@@ -36,40 +42,55 @@ void loop() {
           client.println();
           client.println("<HTML>");
           client.println("<HEAD>");
-          client.println("<TITLE>Arduuino Project</TITLE>");
+          client.println("<TITLE>escControl</TITLE>");
           client.println("<meta http-equiv=\"refresh\" content=\"1\">");
 
           client.println("</HEAD>");
-          client.println("<BODY bgcolor='white'><center>");
-          client.println("<H1 style=\"background-color:white;\">Arduino project</H1>");
-
-          client.println("<a href=\"/?light-\"> - </a> <a href=\"/?light+\"> + </a>");
+          client.println("<body style=background-color:#ADD8E6><center>");
+          client.println("<H1>escControl</H1>");
+          client.println("<input type=\"button\" onclick=\"location.href='?speed-';\" value=\"Decrease\" />");
+          client.println("<input type=\"button\" onclick=\"location.href='?speed+';\" value=\"Increase\" />");
+          //client.println("<a href=\"/?light-\"> - </a> <a href=\"/?light+\"> + </a>");
           client.println(" </center></BODY></HTML>");
           delay(1);
-          //stopping client
           client.stop();
-          /////////////////////
 
+          if (readString.indexOf("?speed-") > 0) //checks for -
+          { spd = spd - 5;
+            if (spd < 0) {
+              spd = 0;
+            }
+            spdEsc = map(spd, 0, 100, 1000, 2000);
+            esc.writeMicroseconds(spdEsc);
+            
+            Serial.println("spd -");
+            Serial.println(spdEsc);
 
-          if (readString.indexOf("?light-") > 0) //checks for -
-          { brightness = brightness - 15;
-            analogWrite(9, brightness);
-            Serial.println("brightness -");
+            delay(1);
+            client.stop();
+            readString = "";
           }
           else
           {
-            if (readString.indexOf("?light+") > 0) //checks for +
-            { brightness = brightness + 15;
-              analogWrite(9, brightness);
-              Serial.println("brightness +");
+            if (readString.indexOf("?speed+") > 0) //checks for +
+            { spd = spd + 5;
+              if (spd > 100) {
+                spd = 100;
+              }
+              spdEsc = map(spd, 0, 100, 1000, 2000);
+              esc.writeMicroseconds(spdEsc);
+
+              Serial.println("spd +");
+              Serial.println(spdEsc);
+
+              delay(1);
+              client.stop();
+              readString = "";
             }
 
-
             delay(1);
-            //stopping client
             client.stop();
             readString = "";
-
           }
         }
       }
