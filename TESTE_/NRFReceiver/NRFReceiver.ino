fd7d17ad3eda8c599a757f1fc717bfdec33b441c
mustaffxx <felipeamorimw@gmail.com>
2020-01-08 15:56:55 -0300

    to download

---
+++ TESTE_/NRFReceiver/NRFReceiver.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+
+RF24 radio(9, 10); // CE, CSN
+const byte address2[6] = "node2";
+
+//
+float rpm = 0.0;
+
+//
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate(RF24_2MBPS);
+  radio.setChannel(125);
+  radio.openReadingPipe(0, address2);
+  radio.startListening();
+}
+void loop() {
+
+  if (radio.available()) {
+    radio.read(&rpm, sizeof(rpm));
+    Serial.print("RPM: ");
+    Serial.println(rpm);
+
+
+    // set the LED with the ledState of the variable:
+    //digitalWrite(3, ledState);
+  }
+}
